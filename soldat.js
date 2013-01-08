203d47d78ae74f3f0fcd63c142cd2418c1ca562a
Jesper Erlandsson <jesper_91@live.se>
2013-01-08 16:26:04 +0100

    Update soldat.js

--- soldat.js
+++ soldat.js
@@ -1,93 +1,173 @@
-function Soldat(name)
+'use strict';
+function Soldat(stage)
 {
-  this.name = name || "random";
-	this.iSoldat = new Image();
-	this.iLoaded = 0;
+	this.images = {over: null, under: null};
+	this.totImagesToLoad = 2;
+	this.numberOfLoadedImages = 0;
+	this.animations = {over: null, under:null};
 	this.ready = false;
-	this.spriteSheet = null;
-	this.animation = null;
-	this.path = "img/running2.png";
-	this.pressing = false;
+	this.soldier = new createjs.Container();
 	this.mode = "run";
 	this.direction = 90;
+	this.way = -1;
+	this.stage = stage;
 }
 Soldat.prototype = {
-	init: function() {
-		this.iSoldat.src = this.path;
-		this.iSoldat.onerror = handleImageError;	
-	    this.iSoldat.onload = this.build.call(this);	
+	init: function(under,over) {
+		this.images.over =over;
+		this.images.under = under;
+		this.build.call(this);
 	},
-	build: function(event) {
-		 var spriteSheet = new createjs.SpriteSheet({
-	  		images: [this.iSoldat],
-	  		frames: {width:33, height:40, regX:20, regY:20},
+	build: function() {
+		//	Create sprites for runing , standing , walking (12 x 3)
+		var underSprite = new createjs.SpriteSheet({
+	  		images: [this.images.under],
+	  		frames: {width:31, height:20, regX:10, regY:10},
 	  		animations: {
-	  			run: [0,11,"run",3],
-	  			idle: [12,14,"idle",14]
+	  			run: [0,5,"run",6],
+	  			idle: [6,8,"idle",20]
 	  		}
 		});
 		
-		console.log(spriteSheet);
 
-		createjs.SpriteSheetUtils.addFlippedFrames(spriteSheet, true, false, false);
+		var overSprite = new createjs.SpriteSheet({
+	  		images: [this.images.over],
+	  		frames: {width:56.5, height:26, regX:13, regY:13},
+	  		animations: {
+	  			shoot: [0,6,"idle",6],
+	  			idle: [7,7,"idle",20]
+	  		}
+		});
+        createjs.SpriteSheetUtils.addFlippedFrames(underSprite, true, false, false);
+		createjs.SpriteSheetUtils.addFlippedFrames(overSprite, true, false, false);
 
-	    this.animation = new createjs.BitmapAnimation(spriteSheet);
-	    this.animation.name = name;
-	    this.animation.direction = 90;
-	  	this.animation.vX = 1.8;
-	  	this.animation.vY = 0;
-	  	this.animation.x = 16;
-	  	this.animation.y = 32;
-	  	this.animation.shadow = new createjs.Shadow("#454", 7, 0, 2);
-	 	this.animation.regX = this.animation.spriteSheet.frameWidth / 2 | 0;
-   		this.animation.regY = this.animation.spriteSheet.frameHeight / 2 | 0;
+	  	this.animations.over = new createjs.BitmapAnimation(overSprite);
+	  	this.animations.over.name = "s";
+	  	this.animations.over.regX = this.animations.over.spriteSheet.frameWidth / 2 | 0;
+   		this.animations.over.regY = this.animations.over.spriteSheet.frameHeight / 2 | 0;
+		
+	  	this.animations.over.x = 0;
+	  	this.animations.over.y = 0;
+	  	this.animations.over.currentFrame = 0;
 
-   		this.animation.currentFrame = 0;
-   		this.addToStage.call(this);
-   		this.animation.gotoAndPlay("run_h");     //runing from left to right
-   		
-   		startGame();
+	  	this.animations.under = new createjs.BitmapAnimation(underSprite);
+	  	this.animations.under.name = "s";
+	  	this.animations.under.regX = this.animations.under.spriteSheet.frameWidth / 2 | 0;
+   		this.animations.under.regY = this.animations.under.spriteSheet.frameHeight / 2 | 0;
+		
+	  	this.animations.under.x =0;
+	  	this.animations.under.y = 18;
+	  	this.animations.under.currentFrame = 0;
+
+	  	this.soldier.addChild(this.animations.under);
+		this.soldier.addChild(this.animations.over);
+
+		this.animations.over.gotoAndPlay("idle"); 
+		this.animations.under.gotoAndPlay("idle"); 
+		
+		this.soldier.x = 100;
+		this.soldier.y = 100;
+		this.soldier.vY = 0;
+		this.soldier.vX = 0;
 	},
-	setDirection: function(dir) {
-		this.animation.direction = dir;
-		if(dir == 90 ) {
-			// we want the sprite to turn right
-				this.animation.gotoAndPlay(this.mode+"_h");     //runing from left to right
+	returnSoldier: function()
+	{
+		return this.soldier;
+	},
+	setDirection: function(mode) {
+
+		if(this.direction == -90 ) {
+			
+			console.log(mode+"_h");
+			
+			this.animations.under.gotoAndPlay(mode+"_h");     //runing from left to right
+			this.animations.over.gotoAndPlay("idle_h");
 		}
-		else if (dir == -90) {
-			this.animation.gotoAndPlay(this.mode);
-			// we want the sprite to turn left
+		else if (this.direction == 90) {
+			
+			this.animations.under.gotoAndPlay(mode);
+			this.animations.over.gotoAndPlay("idle");
+			
 		}
 	},
-	setSpeed: function(speed) {
-		this.animation.vX = speed;
-	  	this.animation.vY = speed;
-	},
-	setState: function(state) {
-		this.animation.gotoAndPlay(state);
-	},
-	addToStage: function() {
-		stage.addChild(this.animation);
-	},
-	setMode: function(mode){
-		this.mode = mode;
-		this.setDirection.call(this,this.animation.direction);
+	getCurrentDirection: function()
+	{
+		return this.direction;
 	},
 	move: function() {
-
-		if (this.animation.direction == 90) {
-           this.animation.x += this.animation.vX;
-           this.animation.y += this.animation.vY;
+		if (this.direction == 90) {
+          	this.soldier.x += this.soldier.vX;
+           this.soldier.y += this.soldier.vY;
         }
         else {
-           this.animation.x -= this.animation.vX;
-           this.animation.y -= this.animation.vY;
+          	this.soldier.x -= this.soldier.vX;
+           this.soldier.y -= this.soldier.vY;
         }	    	
 	},
-	update:function() {
-		if(this.mode != "idle")
+	idle: function(dir) {
+		this.direction = dir;
+		this.soldier.vX = 0;
+		//this.animations.under.gotoAndPlay("idle");
+		this.setDirection.call(this,"idle");
+	},
+	walk: function(dir) {
+		// set speed
+		// set sprite
+		this.soldier.vX = 1;
+		this.animations.under.gotoAndPlay("walk");
+	},
+	run: function(dir) {
+		// set speed
+		// set sprite
+		this.direction = dir;
+		this.soldier.vX = 1.8;
+		this.setDirection.call(this,"run");
+		//this.animations.under.gotoAndPlay("run");
+
+	},
+	shoot: function() {
+		if(this.direction == 90)
 		{
-			this.move();
+			this.animations.over.gotoAndPlay("shoot");
 		}
+		else
+		{
+			this.animations.over.gotoAndPlay("shoot_h");	
+		}
+	},
+	die: function(wayToDie) {
+
+	},
+	update:function() {
+		// code for the rotiation 
+		var cx = this.soldier.x;
+		var cy = this.soldier.y;
+
+		var mx, my = 0;
+    	mx = this.stage.mouseX;
+    	my = this.stage.mouseY;
+    	var angle = Math.atan2(my - cy, mx - cx) * 180 / Math.PI;
+
+    	if(angle < 0) { angle = 360 + angle; }
+
+    	//if(angle < 40 && angle > 0 || angle < 356 && angle > 320)
+    	//{
+
+    	if(this.stage.mouseX < this.soldier.x && this.direction != -90) {
+    		this.idle.call(this,-90);
+    	}
+    	else if(this.stage.mouseX > this.soldier.x && this.direction != 90) {
+    		this.idle.call(this,90);
+    	}
+    	if(this.direction == 90) {
+
+    		this.animations.over.rotation = angle -25;
+    	}
+    	else {	
+    		this.animations.over.rotation = angle +200;
+    	}
+    	
+
+    	this.move();
 	}
 }
