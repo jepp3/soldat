24053e47eb98f834035f71ddbf470d35ea2bd39a
Jesper Erlandsson <jesper_91@live.se>
2013-01-09 14:36:39 +0100

    removed buggs & and drank coffee...yum

--- soldat.js
+++ soldat.js
@@ -1,16 +1,19 @@
 'use strict';
+var DIRECTION = {
+	LEFT: 0,
+	RIGHT: 1
+};
 function Soldat(stage)
 {
 	this.images = {over: null, under: null};
-	this.totImagesToLoad = 2;
-	this.numberOfLoadedImages = 0;
 	this.animations = {over: null, under:null};
-	this.ready = false;
 	this.soldier = new createjs.Container();
-	this.mode = "run";
 	this.direction = 90;
-	this.way = -1;
 	this.stage = stage;
+	this.weight = {x:0,y:8};
+	this.jumping = true;
+	this.dead = false;
+	this.way = DIRECTION.RIGHT;
 }
 Soldat.prototype = {
 	init: function(under,over) {
@@ -24,17 +27,17 @@ Soldat.prototype = {
 	  		images: [this.images.under],
 	  		frames: {width:31, height:20, regX:10, regY:10},
 	  		animations: {
-	  			run: [0,5,"run",6],
+	  			run: [0,5,"run",5],
 	  			idle: [6,8,"idle",20]
 	  		}
 		});
-		
+
 
 		var overSprite = new createjs.SpriteSheet({
 	  		images: [this.images.over],
 	  		frames: {width:56.5, height:26, regX:13, regY:13},
 	  		animations: {
-	  			shoot: [0,6,"idle",6],
+	  			shoot: [0,6,"idle",2],
 	  			idle: [7,7,"idle",20]
 	  		}
 		});
@@ -45,7 +48,7 @@ Soldat.prototype = {
 	  	this.animations.over.name = "s";
 	  	this.animations.over.regX = this.animations.over.spriteSheet.frameWidth / 2 | 0;
    		this.animations.over.regY = this.animations.over.spriteSheet.frameHeight / 2 | 0;
-		
+
 	  	this.animations.over.x = 0;
 	  	this.animations.over.y = 0;
 	  	this.animations.over.currentFrame = 0;
@@ -54,7 +57,7 @@ Soldat.prototype = {
 	  	this.animations.under.name = "s";
 	  	this.animations.under.regX = this.animations.under.spriteSheet.frameWidth / 2 | 0;
    		this.animations.under.regY = this.animations.under.spriteSheet.frameHeight / 2 | 0;
-		
+
 	  	this.animations.under.x =0;
 	  	this.animations.under.y = 18;
 	  	this.animations.under.currentFrame = 0;
@@ -64,11 +67,15 @@ Soldat.prototype = {
 
 		this.animations.over.gotoAndPlay("idle"); 
 		this.animations.under.gotoAndPlay("idle"); 
-		
+
 		this.soldier.x = 100;
 		this.soldier.y = 100;
 		this.soldier.vY = 0;
 		this.soldier.vX = 0;
+	},
+	getPos: function()
+	{
+		return {x:this.soldier.x,y:this.soldier.y};
 	},
 	returnSoldier: function()
 	{
@@ -77,17 +84,12 @@ Soldat.prototype = {
 	setDirection: function(mode) {
 
 		if(this.direction == -90 ) {
-			
-			console.log(mode+"_h");
-			
 			this.animations.under.gotoAndPlay(mode+"_h");     //runing from left to right
 			this.animations.over.gotoAndPlay("idle_h");
 		}
 		else if (this.direction == 90) {
-			
 			this.animations.under.gotoAndPlay(mode);
 			this.animations.over.gotoAndPlay("idle");
-			
 		}
 	},
 	getCurrentDirection: function()
@@ -111,63 +113,99 @@ Soldat.prototype = {
 		this.setDirection.call(this,"idle");
 	},
 	walk: function(dir) {
-		// set speed
-		// set sprite
 		this.soldier.vX = 1;
 		this.animations.under.gotoAndPlay("walk");
 	},
 	run: function(dir) {
-		// set speed
-		// set sprite
 		this.direction = dir;
 		this.soldier.vX = 1.8;
 		this.setDirection.call(this,"run");
 		//this.animations.under.gotoAndPlay("run");
-
 	},
-	shoot: function() {
-		if(this.direction == 90)
-		{
+	reverse: function(dir) {
+		this.direction = dir;
+		this.soldier.vX = 1.8;
+		//this.setDirection.call(this,"run");
+		if(this.direction == -90 ) {
+
+			console.log("revse -90");
+
+			this.animations.under.gotoAndPlay("run");     //runing from left to right
+		}
+		else if (this.direction == 90) {
+
+			console.log("reverse 90");
+			this.animations.under.gotoAndPlay("run_h");
+		}
+	},
+	jump:function(dir) {
+
+		this.direction = dir;
+		this.jumping = true;
+	},
+	shoot: function(dir) {
+		if(dir == 90) {
 			this.animations.over.gotoAndPlay("shoot");
 		}
-		else
-		{
+		else {
 			this.animations.over.gotoAndPlay("shoot_h");	
 		}
 	},
 	die: function(wayToDie) {
-
+		this.soldier.visible = false;
+		this.dead = true;
 	},
 	update:function() {
-		// code for the rotiation 
-		var cx = this.soldier.x;
-		var cy = this.soldier.y;
+		if(this.dead == false) {
+			var cx = this.soldier.x;
+			var cy = this.soldier.y;
 
-		var mx, my = 0;
-    	mx = this.stage.mouseX;
-    	my = this.stage.mouseY;
-    	var angle = Math.atan2(my - cy, mx - cx) * 180 / Math.PI;
+			var mx, my = 0;
+	    	mx = this.stage.mouseX;
+	    	my = this.stage.mouseY;
+	    	var angle = Math.atan2(my - cy, mx - cx) * 180 / Math.PI;
 
-    	if(angle < 0) { angle = 360 + angle; }
+	    	if(angle < 0) { angle = 360 + angle; }
 
-    	//if(angle < 40 && angle > 0 || angle < 356 && angle > 320)
-    	//{
+	    	if(this.jumping == true) {
+	    		this.weight.y = this.weight.y - 0.5;
+				this.soldier.y -= this.weight.y;
+	    	}
 
-    	if(this.stage.mouseX < this.soldier.x && this.direction != -90) {
-    		this.idle.call(this,-90);
-    	}
-    	else if(this.stage.mouseX > this.soldier.x && this.direction != 90) {
-    		this.idle.call(this,90);
-    	}
-    	if(this.direction == 90) {
+	    	if(parseInt(this.soldier.y) == 100) {
+	    		this.weight.y = 8;
+	    		this.jumping = false;
+	    	}
 
-    		this.animations.over.rotation = angle -25;
-    	}
-    	else {	
-    		this.animations.over.rotation = angle +200;
-    	}
-    	
+	  		// OM musen är¨på vänster sida av soldaten  och  vi går åt höger
+	    	if(this.stage.mouseX < this.soldier.x  && Key.isDown(Key.D) ==true) {
+	    			//this.idle.call(this,-90);
+	    			this.animations.over.rotation = angle +200;
+	    	}
+	    	// OM musen är på högersida av soldaten och vi går åt vänster
+	    	else if(this.stage.mouseX > this.soldier.x && Key.isDown(Key.A) ==true) {
+	    			//this.idle.call(this,90);	
+	    			this.animations.over.rotation = angle -25;
+	    	}
+	    	// om musen är på vänster sida, och vi inte trycker på D
+	    	else if(this.stage.mouseX < this.soldier.x  && Key.isDown(Key.D) === undefined) {
+	    		if(this.way != DIRECTION.LEFT) {// om vi inte redan är på vänster sida( byta ut mot enum)
+	    			this.idle.call(this,-90);
+	    			this.way = DIRECTION.LEFT;
+	    		}
+	    		this.animations.over.rotation = angle +200;
+	    	}
+	    	// OM musen är på höger sida och vi intetrycker på A
+	    	else if(this.stage.mouseX > this.soldier.x && Key.isDown(Key.A) === undefined) {
 
-    	this.move();
+	    		if(this.way != DIRECTION.RIGHT) {// om vi inte redan är på höger sida
+	    			this.idle.call(this,90);
+	    			this.way = DIRECTION.RIGHT;
+	    		}
+	    		//this.idle.call(this,90);    		
+	    		this.animations.over.rotation = angle -25;
+	    	}
+	    	this.move();
+	    }
 	}
 }
