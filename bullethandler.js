944948b8e9c5ad52d30ef61aa129def625e837af
Jesper Erlandsson <jesper_91@live.se>
2013-01-14 04:59:11 -0800

    simple container for bullets

---
+++ bullethandler.js
@@ -0,0 +1,36 @@
+function BulletHandler (max,stage) {
+
+  this.num = 0;
+	this.bullets = new Array();
+	this.max = max; 
+	this.stage = stage;
+}
+
+BulletHandler.prototype = {
+
+	add: function(bullet)
+	{
+		if(this.num == this.max)
+		{
+			num = 0;
+		}
+		this.bullets[this.num] = bullet;
+		this.bullets[this.num].init();
+		stage.addChild(this.bullets[this.num].shape);
+
+		this.num++;
+	},
+	at: function(i)
+	{
+		return this.bullets[i];
+	},
+	getIndex: function()
+	{
+		return this.num;
+	},
+	last: function()
+	{
+		console.log(this.bullets)
+		return this.bullets[this.num-1];
+	}
+}
